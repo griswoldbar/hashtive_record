43372d8259bad0c1510b38751992dd1e0fbee0ed
Christopher Turtle <chris.turtle@onapp.com>
2012-11-16 10:31:03 +0000

    nothing

--- spec/hashtive_record_spec.rb
+++ spec/hashtive_record_spec.rb
@@ -7,18 +7,22 @@ describe "HashtiveRecord" do
     @players = build(:table, id: :players)
     @races = build(:table, id: :races)
     @matches = build(:table, id: :matches)
-
+    @dorks = build(:table, id: :dorks)
+    
     @lineker_rec = build(:record, hash: {lineker: {event_id: :footie_match, event_type: :match }})
     @christie_rec = build(:record, hash: {christie: {event_id: :running_race, event_type: :race }})
     @arsenal_rec = build(:record, hash: {footie_match: {stadium: "Arsenal"} } )
     @olympic_rec = build(:record, hash: {running_race: {stadium: "Olympic"} } )
-    @players << @lineker_rec
-    @players << @christie_rec
+    @plonker_rec = build(:record, hash: {plonker: {name: 'Winky'}})
+    @wanky_rec = build(:record, hash: {wanky: {name: 'Dorkus'}})
+    
+    @players << @lineker_rec << @christie_rec
     @races << @arsenal_rec
     @matches << @olympic_rec
+    @dorks << @plonker_rec << @wanky_rec
     
     @database = build(:database)
-    @database.tables << @players << @races << @matches
+    @database.tables << @players << @races << @matches << @dorks
     HashtiveRecord::Base.database = @database
     
     module Event
@@ -26,8 +30,17 @@ describe "HashtiveRecord" do
       included { has_many :players, as: :event }
     end
     
+    module Hero;end
+    module God;end
+    class Dork < HashtiveRecord::Base
+      belongs_to :master, class_name: :player 
+      belongs_to :overlord, class_name: :player
+    end
+    
     class Player < HashtiveRecord::Base
       belongs_to :event, polymorphic: true
+      has_many :slaves, class_name: :dork
+      has_many :eunuchs, class_name: :dork
     end
     
     class Race < HashtiveRecord::Base
@@ -39,21 +52,38 @@ describe "HashtiveRecord" do
     end
   end
   
-  
-  before(:each) do
-    @lineker = Player.instantiate(@lineker_rec)
-    @christie = Player.instantiate(@christie_rec)
-    @arsenal = Match.instantiate(@arsenal_rec)
-    @olympic = Race.instantiate(@olympic_rec)
+  describe "polymorphics" do
+    before(:each) do
+      @lineker = Player.instantiate(@lineker_rec)
+      @christie = Player.instantiate(@christie_rec)
+      @arsenal = Match.instantiate(@arsenal_rec)
+      @olympic = Race.instantiate(@olympic_rec)
+    end
+
+    it "works" do
+      @arsenal.players.map(&:record).should =~ [@lineker_rec]
+      @arsenal.players << @christie
+      @arsenal.players.map(&:record).should =~ [@lineker_rec, @christie_rec]
+      @lineker.event = @olympic
+      @arsenal.players.map(&:record).should =~ [@christie_rec]
+      @olympic.players.map(&:record).should =~ [@lineker_rec]
+    end
   end
-  
-  it "works" do
-    @arsenal.players.map(&:record).should =~ [@lineker_rec]
-    @arsenal.players << @christie
-    @arsenal.players.map(&:record).should =~ [@lineker_rec, @christie_rec]
-    @lineker.event = @olympic
-    @arsenal.players.map(&:record).should =~ [@christie_rec]
-    @olympic.players.map(&:record).should =~ [@lineker_rec]
-  end
+
+  # describe "multi relationships" do
+  #   before(:each) do
+  #     @lineker = Player.instantiate(@lineker_rec)
+  #     @christie = Player.instantiate(@christie_rec)
+  #     @wanky = Dork.instantiate(@wanky_rec)
+  #     @plonker = Dork.instantiate(@plonker_rec)
+  #   end
+  #   
+  #   it "works" do
+  #     @wanky.hero = @lineker
+  #     @plonker.hero = @lineker
+  #     @lineker.heroes.should resemble([@wanky,@dorky])
+  #   end
+  # end
+
   
 end
