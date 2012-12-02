83ab820375df90491efdc2b48eeb808fc3c71c4a
Christopher Turtle <chris@onapp.com>
2012-12-02 17:56:33 +0000

    added name and description to modifier

--- lib/hashtive_record/modifier.rb
+++ lib/hashtive_record/modifier.rb
@@ -1,16 +1,19 @@
 module HashtiveRecord
   module Modifier
-    def set_message(text, mod_name, thing_name=nil)
-      text.gsub!(/==placeholder==/, thing_name) if thing_name
-      reader = "#{mod_name}_message"
-      instance_variable_set("@"+reader, text)
 
+    def define_reader(attribute, text, mod_name, thing_name=nil)
+
+      text = text.gsub(/==placeholder==/, thing_name) if thing_name
+      reader = "#{mod_name}_#{attribute}"
+      instance_variable_set("@"+reader, text)
+    
       instance_eval "
         def #{reader}
           @#{reader}
         end
       "
     end
+
 
     def self.included(base)
       base.extend(ClassMethods)
@@ -20,25 +23,41 @@ module HashtiveRecord
     module ClassMethods
       def setup_defaults
         mod_name = self.to_s.downcase
-        text = self.message
-        instance_eval "
-        def self.extended(thing)
-          if thing.modifier_modules.include?(:#{mod_name}) && thing.record.modifiers.#{mod_name} && thing.record.modifiers.#{mod_name}.message
-            text = thing.record.modifiers.#{mod_name}.message
-            thing.set_message text, %Q{#{mod_name}}
-          else
-            thing.set_message %Q{#{text}}, %Q{#{mod_name}}, thing.name
+        message_text = self.message
+        name_text = self.new_name
+        description_text = self.new_description
+        
+        define_singleton_method :extended do |thing|
+          [:message, :description, :name].each do |attribute|
+            if thing.modifier_modules.include?(mod_name.to_sym) && thing.record.modifiers.send(mod_name) && thing.record.modifiers.send(mod_name).send(attribute)
+              text = thing.record.modifiers.send(mod_name).send(attribute)
+              thing.define_reader attribute, text, "#{mod_name}"
+            else
+              thing.define_reader attribute, eval("#{attribute}_text"), "#{mod_name}", thing.name
+            end
           end
+
         end
-        "
       end
       
       def message
-        @message || "You #{name.downcase} the ==placeholder=="
+        "You #{name.downcase} the ==placeholder=="
       end
       
-      def default_message(text)
-        @message = text
+      def new_name
+        "#{name.downcase}ed ==placeholder=="
+      end
+      
+      def new_description
+        "the ==placeholder== has been #{name.downcase}ed"
+      end
+      
+      def defaults(options)
+        options.each {|k,v|
+          define_singleton_method k do
+            v
+          end
+        }
         setup_defaults
       end
     end
--- spec/modifier_spec.rb
+++ spec/modifier_spec.rb
@@ -3,7 +3,11 @@ require 'spec_helper'
 
 
 describe "Modifier" do
-
+  let(:thing)  { build(:thing) }
+  let(:record) { build(:thing_record, modifiers: {plonk: {message: "You fuck it up big time", 
+                                                          name: "thingumy blah",
+                                                          description: "it looks awful"}})}
+  
   before(:all) do
     module Destroy
       include HashtiveRecord::Modifier      
@@ -11,15 +15,16 @@ describe "Modifier" do
     
     module Plonk
       include HashtiveRecord::Modifier
-      default_message "Oh bugger"
+      defaults message: "Oh bugger", 
+               new_name: "wankered thing",
+               new_description: "it's completely fucked"
+                 
     end
     
   end
   
   describe "#message" do
-    let(:thing)  { build(:thing) }
-    let(:record) { build(:thing_record, modifiers: {plonk: {message: "You fuck it up big time"}})}
-    
+
     it "outputs the default message if none is given at module level" do
       thing.add_modifier(:destroy)
       thing.destroy_message.should == "You destroy the #{thing.name}"
@@ -35,4 +40,41 @@ describe "Modifier" do
       thing.plonk_message.should == "You fuck it up big time"
     end
   end
+  
+  describe "#name" do
+    it "outputs the default name if none is given at module level" do
+      thing.add_modifier(:destroy)
+      thing.destroy_name.should == "destroyed #{thing.name}"
+    end
+    
+    it "outputs the modules's default name if none is given at instance level" do
+      original_name = thing.name
+      thing.add_modifier(:plonk)
+      thing.plonk_name.should == "wankered thing"
+    end
+    
+    it "outputs the instance's name if supplied" do
+      thing = record.klass.instantiate(record)
+      thing.plonk_name.should == "thingumy blah"
+    end
+  end
+  
+  describe "#description" do
+    it "outputs the default description if none is given at module level" do
+      thing.add_modifier(:destroy)
+      thing.destroy_description.should == "the #{thing.name} has been destroyed"
+    end
+    
+    it "outputs the modules's default description if none is given at instance level" do
+      original_description = thing.description
+      thing.add_modifier(:plonk)
+      thing.plonk_description.should == "it's completely fucked"
+    end
+    
+    it "outputs the instance's description if supplied" do
+      thing = record.klass.instantiate(record)
+      thing.plonk_description.should == "it looks awful"
+    end
+  end
+  
 end
